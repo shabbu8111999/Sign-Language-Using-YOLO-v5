ca833232600db96c761efaa3d404c7cf004de872
Shabareesh Nair <shabareesh08@gmail.com>
2025-07-03 16:42:22 +0300

    notebook updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Sign Language Detection",
+    version = "0.0.0",
+    author = "Shabareesh Nair",
+    author_email = "shabareesh08@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
